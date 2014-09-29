8cffcf3f7d74c30fbc5bfa7399d4afbdd73019f5
Eddie <eddie@Eddies-MacBook-Pro.local>
2014-09-28 21:30:09 -0600

    ver1.2

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,7 +71,9 @@ Appraisal_fee.create! \
   fee:         "$50" 
   
   
-                     
+Admin.create! \
+  username: "mark", password: "mR4ll*A7ee8", 
+  password_confirmation: "mR4ll*A7ee8"                 
 
 
 
