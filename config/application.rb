176cf2ae5ea8bf48dada69691cbde69170d28c07
Eddie <eddie@Eddies-MacBook-Pro.local>
2014-12-03 18:45:09 -0700

    config.v2.1

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,8 @@
+
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
-config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-}
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,6 +13,7 @@ module App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_dispatch.default_headers.merge!({'X-Frame-Options' => ''})
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -24,4 +23,6 @@ module App
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
-end
+ end
+
+
